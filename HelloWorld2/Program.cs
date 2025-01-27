0154f1379f437ace20c1f479b19dcad1de74804a
Dacheng Gao <successgdc@gmail.com>
2025-01-27 15:47:36 +0800

    add HelloWorld2
    Using implicitly modifiers for class and method:
    internal for class;
    private for method;

---
+++ HelloWorld2/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloWorld2;
+
+// Implicitly to be `internal` for class
+class Program
+{
+    // Implicitly to be `private` for method
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
