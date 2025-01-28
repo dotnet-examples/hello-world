69a0a6a6e7f04aa2dd1b439c877b0ba5c932f434
Dacheng Gao <successgdc@gmail.com>
2025-01-28 11:24:53 +0800

    add HelloWorld6
    using top-level statements

---
+++ HelloWorld6/Program.cs
@@ -0,0 +1,12 @@
+﻿// The simple helloworld application,
+// using top-level statements.
+
+// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("args count: " + args.Length);
+
+await Task.CompletedTask;
+
+return 0;
