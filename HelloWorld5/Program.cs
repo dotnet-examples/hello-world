bc51578c821778b2eb2d39a396ae5cb1ac85550f
Dacheng Gao <successgdc@gmail.com>
2025-01-28 11:10:57 +0800

    add HelloWorld5
    use async / await;

---
+++ HelloWorld5/Program.cs
@@ -0,0 +1,14 @@
+﻿// The simplist helloworld application,
+// with async / await support.
+
+namespace HelloWorld5;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.WriteLine("Hello, World!");
+
+        await Task.CompletedTask;
+    }
+}
